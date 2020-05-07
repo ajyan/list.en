2bed14c6edf41b57b80d5072751b1a63266c6b19
Andrew Jyan <andrew.jyan4@gmail.com>
2020-05-07 10:22:32 -0400

    added playlists

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
+import SpotifyWebApi from 'spotify-web-api-js';
+const spotifyApi = new SpotifyWebApi();
 
 class App extends Component {
   constructor() {
     super();
     const params = this.getHashParams();
-    console.log(params);
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: 'Not Checked', albumArt: '' },
+      playlists: [],
+      playlistIndex: 0,
+    };
   }
+
+  getUserPlaylists() {
+    spotifyApi
+      .getUserPlaylists() // note that we don't pass a user id
+      .then(({ items }) => {
+        this.setState({ playlists: items });
+        console.log('User playlists', items);
+      })
+      .catch((err) => {
+        console.log('Error retrieving playlists', err);
+      });
+  }
+
+  getNowPlaying() {
+    spotifyApi.getMyCurrentPlaybackState().then((response) => {
+      console.log('this', response);
+      this.setState({
+        nowPlaying: {
+          name: response.item.name,
+          albumArt: response.item.album.images[0].url,
+        },
+      });
+    });
+  }
+
   getHashParams() {
     var hashParams = {};
     var e,
@@ -23,6 +59,27 @@ class App extends Component {
     return (
       <div className="App">
         <a href="http://localhost:8888"> Login to Spotify </a>
+        <div>Now Playing: {this.state.nowPlaying.name}</div>
+        <div>
+          <img src={this.state.nowPlaying.albumArt} />
+        </div>
+        {this.state.loggedIn && (
+          <button
+            onClick={() => {
+              // this.getNowPlaying();
+              this.getUserPlaylists();
+            }}
+          >
+            Check now playing
+          </button>
+        )}
+        {this.state.playlists.map((playlist, i) => {
+          return (
+            <div id={i} value={i}>
+              {playlist.name}
+            </div>
+          );
+        })}
       </div>
     );
   }
